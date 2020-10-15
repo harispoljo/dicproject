eed88720e0f0a1851626dbbfea3c20438307a811
harispoljo <34479922+harispoljo@users.noreply.github.com>
2020-10-15 16:01:14 +0200

    initial link between api and kafka

---
+++ middleman/Middleman.scala
@@ -0,0 +1,80 @@
+package generator
+
+import java.util.{Date, Properties}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
+import scala.util.Random
+import kafka.producer.KeyedMessage
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.JsonNode
+
+object ScalaProducerExample extends App {
+
+    /**
+      * Returns the text (content) from a REST URL as a String.
+      * Inspired by http://matthewkwong.blogspot.com/2009/09/scala-scalaiosource-fromurl-blockshangs.html
+      * and http://alvinalexander.com/blog/post/java/how-open-url-read-contents-httpurl-connection-java
+      *
+      * The `connectTimeout` and `readTimeout` comes from the Java URLConnection
+      * class Javadoc.
+      * @param url The full URL to connect to.
+      * @param connectTimeout Sets a specified timeout value, in milliseconds,
+      * to be used when opening a communications link to the resource referenced
+      * by this URLConnection. If the timeout expires before the connection can
+      * be established, a java.net.SocketTimeoutException
+      * is raised. A timeout of zero is interpreted as an infinite timeout.
+      * Defaults to 5000 ms.
+      * @param readTimeout If the timeout expires before there is data available
+      * for read, a java.net.SocketTimeoutException is raised. A timeout of zero
+      * is interpreted as an infinite timeout. Defaults to 5000 ms.
+      * @param requestMethod Defaults to "GET". (Other methods have not been tested.)
+      *
+      * @example get("http://www.example.com/getInfo")
+      * @example get("http://www.example.com/getInfo", 5000)
+      * @example get("http://www.example.com/getInfo", 5000, 5000)
+      */
+    @throws(classOf[java.io.IOException])
+    @throws(classOf[java.net.SocketTimeoutException])
+    def get(url: String,
+            connectTimeout: Int = 5000,
+            readTimeout: Int = 5000,
+            requestMethod: String = "GET") =
+    {
+        import java.net.{URL, HttpURLConnection}
+        val connection = (new URL(url)).openConnection.asInstanceOf[HttpURLConnection]
+        connection.setConnectTimeout(connectTimeout)
+        connection.setReadTimeout(readTimeout)
+        connection.setRequestMethod(requestMethod)
+        val inputStream = connection.getInputStream
+        val content = io.Source.fromInputStream(inputStream).mkString
+        if (inputStream != null) inputStream.close
+        content
+    }
+
+    val topic = "corona"
+    val brokers = "localhost:9092"
+
+    val props = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+    props.put(ProducerConfig.CLIENT_ID_CONFIG, "Scala coronaAPI middleman")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.springframework.kafka.support.serializer.JsonSerializer")
+    val producer = new KafkaProducer[String, JsonNode](props)
+
+    try {
+        val slug = "south-africa"
+        val url = "https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-04-01T00:00:00Z&to=2020-04-02T00:00:00Z"
+        val content = get(url)
+        val mapper = new ObjectMapper()
+        val node = mapper.readTree(content);
+        val data = new ProducerRecord[String, JsonNode](topic, slug, node)
+        producer.send(data)
+        print(data)
+
+    } catch {
+        case ioe: java.io.IOException =>  // handle this
+        case ste: java.net.SocketTimeoutException => // handle this
+    }
+
+
+    producer.close()
+}
---
+++ middleman/build.sbt
@@ -0,0 +1,10 @@
+name := "Middleman"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+"org.apache.kafka" % "kafka_2.11" % "1.0.0",
+"org.springframework.kafka" % "spring-kafka" % "2.2.14.RELEASE"
+)
